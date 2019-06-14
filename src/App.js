fa0da021c20800f2284526f8f0f306ffc3184d11
AjaySinghSengar <assengar1984@yahoo.com>
2019-06-15 03:50:51 +0530

    Update App.js
    test

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App </h1>
+        <h1>Hi, I'm a React App </h1>
 
         <div>
           <h1>Hi, I'm a React App </h1>
         </div>
-        
+
       </div>
     );
   }
