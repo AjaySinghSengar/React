bb8d8639ea5d7192137b6f43a48162ee24b9f5fe
AjaySinghSengar <assengar1984@yahoo.com>
2019-06-15 11:49:51 +0530

    Props & States

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Ajay', age: 32},
+      {name: 'Parul', age: 28},
+      {name: 'Anshuman', age: 5},
+    ]
+  }
   render() {
      return (
        <div className="App">
          <h1>Hi, I'm a React App </h1>
+         <Person name={this.state.persons[0].name} 
+                 age={this.state.persons[0].age} 
+                  />
+         <Person name='Anshuman' age='4'> Enjoy playing ! </Person>
        </div>
      );
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,14 @@
-const person = () => {
+import React from 'react';
+
+const person = (props) => {
+    return (
+        <div>
+            <p>I am {props.name} and my age is {props.age} !</p>
+            <p> {props.children} </p>
+        </div>
+    )
+    
     
 }
+
+export default person;
