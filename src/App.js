3d49924d116b51660d6672c267de8672df250972
AjaySinghSengar <assengar1984@yahoo.com>
2019-06-15 10:56:29 +0530

    vscode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App </h1>
-    //   </div>
-    // );
+     return (
+       <div className="App">
+         <h1>Hi, I'm a React App </h1>
+       </div>
+     );
 
-    return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does it work ?'));
+    //return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Does it work ?'));
   }
 }
 export default App;
