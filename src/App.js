304bd4787ccd65abc91fdc202cc19560e250f5f9
AjaySinghSengar <assengar1984@yahoo.com>
2019-06-15 13:25:38 +0530

    Render method gets called when state is updated.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,40 @@ class App extends Component {
       {name: 'Ajay', age: 32},
       {name: 'Parul', age: 28},
       {name: 'Anshuman', age: 5},
-    ]
+    ],
+    otherState: 'test'
   }
+
+  switchNameHandler = () => {
+    console.log(this.state);
+    
+    this.setState(
+      {
+        persons: [
+          {name: 'Ajay Singh', age: 32},
+          {name: 'Parul', age: 28},
+          {name: 'Anshuman', age: 5},
+        ]
+      }
+    )
+    console.log(this.state);
+  }
+
   render() {
      return (
        <div className="App">
+         <button onClick={this.switchNameHandler}>Switch Name</button>
+
          <h1>Hi, I'm a React App </h1>
          <Person name={this.state.persons[0].name} 
                  age={this.state.persons[0].age} 
                   />
-         <Person name='Anshuman' age='4'> Enjoy playing ! </Person>
+          <Person name={this.state.persons[1].name} 
+                 age={this.state.persons[1].age} 
+                  />
+          <Person name={this.state.persons[2].name} 
+                 age={this.state.persons[2].age} 
+                  />
        </div>
      );
 
