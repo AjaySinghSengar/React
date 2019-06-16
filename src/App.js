2666cb5db0cb0f46733bd4a5ff33bf38cd3cc02f
AjaySinghSengar <assengar1984@yahoo.com>
2019-06-16 14:19:30 +0530

    List & Ksy, flexible list

--- src/App.js
+++ src/App.js
@@ -5,31 +5,46 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Ajay', age: 32 },
-      { name: 'Parul', age: 28 },
-      { name: 'Anshuman', age: 5 }
+      { name: 'Ajay', age: 32   , id: 'qwqw' },
+      { name: 'Parul', age: 28  , id: 'qed' },
+      { name: 'Anshuman', age: 5, id: 'qewqe' }
     ],
     otherState: 'test',
     showPersons: false
   }
+  
+  deletePersonsHandler = (personIndex) => {
+    //Array is referance type in JS, directaly calling the splice on the 
+    //original array is the bad idea. slice is the method which returns a copy.
+    //const persons = this.state.persons.slice();
+    // Alternative is spred array
+    const persons = [...this.state.persons];
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 32 },
-        { name: 'Parul Singh', age: 28 },
-        { name: 'Anshuman Singh', age: 5 }
-      ]
-    })
+    persons.splice(personIndex,1);
+    this.setState({ persons: persons });
   }
-  onChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Ajay', age: 32 },
-        { name: event.target.value, age: 28 },
-        { name: 'Anshuman', age: 5 }
-      ]
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p=>{
+      return p.id === id;
     });
+
+    // JS object is a referance type
+    //const person = this.state.persons[personIndex];
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //alternative approch
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({persons:persons});
   }
 
   togglePersonsHandler = () => {
@@ -50,15 +65,20 @@ class App extends Component {
     let persons = null;
     if (this.state.showPersons) {
       persons = (
-      <div>
-        {
-          this.state.persons.map(person => {
-            return <Person
-              name={person.name}
-              age={person.name}
-            />
-          })}
-      </div>
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return <Person
+                click={() => this.deletePersonsHandler(index)}
+                //Altersnative of above line
+                //click={this.deletePersonsHandler.bind(this,index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            })}
+        </div>
       );
     }
 
